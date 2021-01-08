11f4105ce7efcdea44ff96512c1cd845f890456b
phanijsp <phani.jsp@gmail.com>
2021-01-08 15:02:55 +0530

    - Renamed project from 'demo' to 'torrentsearch'

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,18 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.http.HttpClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class DemoApplicationTests {
 
     @Autowired
@@ -22,6 +14,7 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        restTemplate.getForObject("http://localhost:8080/", String.class);
+        String response = restTemplate.getForObject("http://localhost:8080", String.class);
+//        assertThat(response).contains("GXeetings");
     }
 }
